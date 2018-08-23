e57704addfaad4e3728b960d7d4580e29f837d2c
Richard Johnson <raj@cisco.com>
2018-08-23 12:26:55 -0700

    Initial commit of my simple menu test code for Fitbit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+/*
+ * Entry point for the watch app
+ */
+
+console.log("App Started");
+
+import document from "document";
+
+let list = document.getElementById("my-list");
+let items = list.getElementsByClassName("tile-list-item");
+
+items.forEach((element, index) => {
+  let touch = element.getElementById("touch-me");
+  touch.onclick = (evt) => {
+    console.log(`touched: ${index} > ${element.text}`);
+  }
+});
